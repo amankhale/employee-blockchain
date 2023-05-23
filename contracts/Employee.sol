7ef59ea6592643552153572c77a086a2d2254c26
akhaleDeqode <akhale@deqode.com>
2023-05-23 11:00:30 +0530

    Updated: contract function

--- contracts/Employee.sol
+++ contracts/Employee.sol
@@ -15,18 +15,7 @@ contract Employee {
         string remarks;
     }
 
-    event NewEmployee(
-        string indexed id,
-        string firstName,
-        string lastName,
-        string email,
-        string date,
-        string education,
-        string designation,
-        string[] techStack,
-        string location,
-        string remarks
-    );
+    event NewEmployee(EmployeeData);
 
     mapping(string => EmployeeData) public employeeList;
     uint public employeeCount = 0;
@@ -34,17 +23,6 @@ contract Employee {
     function addEmployee(EmployeeData memory _employee) public payable {
         employeeList[_employee.id] = _employee;
         employeeCount++;
-        emit NewEmployee(
-            _employee.id,
-            _employee.firstName,
-            _employee.lastName,
-            _employee.email,
-            _employee.date,
-            _employee.designation,
-            _employee.education,
-            _employee.techStack,
-            _employee.location,
-            _employee.remarks
-        );
+        emit NewEmployee(_employee);
     }
 }
