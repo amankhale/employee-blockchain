bee97597c0e3d31371885eb0013ab5648fbd9ab3
akhaleDeqode <akhale@deqode.com>
2023-05-24 18:14:36 +0530

    Completed: delete employee function

--- contracts/Employee.sol
+++ contracts/Employee.sol
@@ -10,19 +10,66 @@ contract Employee {
         string date;
         string education;
         string designation;
-        string[3] techStack;
+        string[] techStack;
         string location;
         string remarks;
     }
 
     event NewEmployee(EmployeeData);
+    EmployeeData[] employeeList;
 
-    mapping(uint => EmployeeData) public employeeList;
     uint public employeeCount = 0;
 
+    function getEmployeeList() public view returns (EmployeeData[] memory) {
+        return employeeList;
+    }
+
     function addEmployee(EmployeeData memory _employee) public payable {
-        employeeList[employeeCount] = _employee;
+        employeeList.push(_employee);
         employeeCount++;
         emit NewEmployee(_employee);
+    }
+
+    function editEmployee(EmployeeData memory _employee) public payable {
+        for (uint256 index = 0; index < employeeCount; index++) {
+            if (compareString(employeeList[index].id, _employee.id)) {
+                employeeList[index] = _employee;
+                break;
+            }
+        }
+    }
+
+    function deleteEmployee(string memory _id) public payable {
+        for (uint256 index = 0; index < employeeCount; index++) {
+            if (compareString(employeeList[index].id, _id)) {
+                deleteArrayElement(index);
+                break;
+            }
+        }
+    }
+
+    function compareString(
+        string memory a,
+        string memory b
+    ) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+
+    function deleteArrayElement(uint _indexOfElement) public payable {
+        if (_indexOfElement >= employeeCount) return;
+        if (_indexOfElement == employeeList.length - 1) {
+            employeeList.pop();
+            employeeCount--;
+            return;
+        }
+        for (
+            uint256 index = _indexOfElement;
+            index < employeeList.length - 1;
+            index++
+        ) {
+            employeeList[index] = employeeList[index + 1];
+        }
+        employeeList.pop();
+        employeeCount--;
     }
 }
